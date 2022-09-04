f6b067c226400a3cb319cbcdc7d44d8b31a6ebb1
pureZer <112064059+JiabZhu@users.noreply.github.com>
2022-09-04 18:42:33 +0800

    rsvp_offline

---
+++ paradigms/rsvp_offline.py
@@ -0,0 +1,8 @@
+
+
+class RSVP_Offline:
+    def __init__(self):
+        super(RSVP_Offline, self).__init__()
+        self.__device = []
+        self.start_time = []
+        self.sti_setting = {}
