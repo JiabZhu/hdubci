3dffe36dc60c93f4f714f3ef6273cd013a7ecc9c
pureZer <9431949+pureZer@user.noreply.gitee.com>
2022-09-05 13:51:28 +0800

    test

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
 import time
+import numpy as np
 from devices.neuroscan import NeuroScan
 
 if __name__ == '__main__':
-    n = NeuroScan()
-    n.connect("10.1.125.103", 4000)
-    print(n.basic_info)
+    a = [1, 2, 3, 4]
+    b = [5, 6, 7, 8]
+    a.extend(b)
+    print(a)
+    np.random.shuffle(a)
+    print(a)
 
-    print("开始播放")
-    n.startAcquisition()
-    time.sleep(10)
-    print("请求发送数据")
-    n.startSendData()
-    time.sleep(10)
-
-    n.stopSendData()
-    time.sleep(0.1)
-    n.stopAcquisition()
-    time.sleep(0.1)
-    n.disconnect()
+    # n = NeuroScan()
+    # n.connect("10.1.125.103", 4000)
+    # print(n.basic_info)
+    #
+    # print("开始播放")
+    # n.startAcquisition()
+    # time.sleep(10)
+    # print("请求发送数据")
+    # n.startSendData()
+    # time.sleep(10)
+    #
+    # n.stopSendData()
+    # time.sleep(0.1)
+    # n.stopAcquisition()
+    # time.sleep(0.1)
+    # n.disconnect()
