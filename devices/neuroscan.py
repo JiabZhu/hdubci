8b1e44b37423533fca3d7b267987082fc0d215ca
pureZer <112064059+JiabZhu@users.noreply.github.com>
2022-09-07 09:48:03 +0800

    neuroscan

--- devices/neuroscan.py
+++ devices/neuroscan.py
@@ -4,6 +4,7 @@ import struct
 import numpy as np
 from threading import Thread
 
+
 class NeuroScan:
     def __init__(self):
         super(NeuroScan, self).__init__()
@@ -15,73 +16,70 @@ class NeuroScan:
         self.__EEG_data = np.array([])
         self.__recv_data_thread = None
 
+    def __set_packet_head(self, header_info):
+        self.__packet_header = {'m_chId': header_info[0], 'm_wCode': header_info[1],
+                                'm_wRequest': header_info[2], 'm_dwSize': header_info[3]}
 
+    def __set_basic_info(self, basis_info):
+        self.basic_info = {'dwSize': basis_info[0], 'nEegChan': basis_info[1], 'nEvtChan': basis_info[2],
+                           'nBlockPnts': basis_info[3], 'nRate': basis_info[4], 'nDataSize': basis_info[5],
+                           'fResolution': basis_info[6]}
 
-    def __setPacketHead(self, tuple):
-        self.__packet_header = {'m_chId': tuple[0], 'm_wCode': tuple[1],
-                                'm_wRequest': tuple[2], 'm_dwSize': tuple[3]}
-
-    def __setBasicInfo(self, tuple):
-        self.basic_info = {'dwSize': tuple[0], 'nEegChan': tuple[1], 'nEvtChan': tuple[2],
-                           'nBlockPnts': tuple[3], 'nRate': tuple[4], 'nDataSize': tuple[5],
-                           'fResolution': tuple[6]}
-
-    def sendCmd(self, m_chId, m_wCode, m_wRequest):
-        header = struct.pack(">4sHHI", m_chId, m_wCode, m_wRequest, 0)
+    def send_cmd(self, m_chid, m_wcode, m_wrequest):
+        header = struct.pack(">4sHHI", m_chid, m_wcode, m_wrequest, 0)
         self.__sock.send(header)
 
     def connect(self, ip, port):
         self.__sock.connect((ip, port))
-        self.getEDFHeader()
+        self.get_edf_header()
 
     def disconnect(self):
-        self.sendCmd(b'CTRL', 1, 2)
+        self.send_cmd(b'CTRL', 1, 2)
         self.__sock.close()
 
-    def startAcquisition(self):
-        self.sendCmd(b'CTRL', 2, 1)
+    def start_acquisition(self):
+        self.send_cmd(b'CTRL', 2, 1)
 
-    def stopAcquisition(self):
-        self.sendCmd(b'CTRL', 2, 2)
+    def stop_acquisition(self):
+        self.send_cmd(b'CTRL', 2, 2)
 
-    def startImpedance(self):
-        self.sendCmd(b'CTRL', 2, 3)
+    def start_impedance(self):
+        self.send_cmd(b'CTRL', 2, 3)
 
-    def startSendData(self):
-        self.sendCmd(b'CTRL', 3, 3)
+    def start_send_data(self):
+        self.send_cmd(b'CTRL', 3, 3)
         self.__f_recvData = True
-        self.__recv_data_thread = Thread(target=self.recvEEGData)
+        self.__recv_data_thread = Thread(target=self.recv_eeg_data)
         self.__recv_data_thread.start()
 
-    def stopSendData(self):
+    def stop_send_data(self):
         self.__f_recvData = False
         time.sleep(0.2)
-        self.sendCmd(b'CTRL', 3, 4)
+        self.send_cmd(b'CTRL', 3, 4)
 
-    def getEDFHeader(self):
-        self.sendCmd(b'CTRL', 3, 5)
+    def get_edf_header(self):
+        self.send_cmd(b'CTRL', 3, 5)
         buffer = self.__sock.recv(12)
 
-
-        self.__setPacketHead(struct.unpack('>4sHHI', buffer))
+        self.__set_packet_head(struct.unpack('>4sHHI', buffer))
 
         buffer = b''
         body_len = self.__packet_header['m_dwSize']
         if body_len > 0:
-            while (len(buffer) < body_len):
+            while len(buffer) < body_len:
                 buffer += self.__sock.recv(body_len - len(buffer))
-        self.__setBasicInfo(struct.unpack('<6If', buffer))
+        self.__set_basic_info(struct.unpack('<6If', buffer))
 
-    def recvEEGData(self):
+    def recv_eeg_data(self):
         while self.__f_recvData:
             buffer = self.__sock.recv(12)
 
-            self.__setPacketHead(struct.unpack('!4sHHI', buffer))
+            self.__set_packet_head(struct.unpack('!4sHHI', buffer))
 
             buffer = b''
             body_len = self.__packet_header['m_dwSize']
             if body_len > 0:
-                while (len(buffer) < body_len):
+                while len(buffer) < body_len:
                     buffer += self.__sock.recv(body_len - len(buffer))
                 data_len = body_len / self.basic_info['nDataSize']
 
@@ -94,3 +92,6 @@ class NeuroScan:
                 else:
                     self.__EEG_data = np.append(self.__EEG_data, data, axis=0)
         self.__EEG_data = self.__EEG_data.transpose()
+
+    def save_eeg_data(self):
+        pass
