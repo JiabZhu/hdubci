2ac0accd2398ab201f882084a8081834455ecbea
pureZer <112064059+JiabZhu@users.noreply.github.com>
2022-09-09 14:37:31 +0800

    rsvponline

--- paradigms/rsvponline.py
+++ paradigms/rsvponline.py
@@ -6,6 +6,8 @@ from threading import Thread
 import util.config as config
 from devices.neuroscan import NeuroScan
 
+from model.model import Net
+
 
 class RsvpOnline:
     def __init__(self):
@@ -15,7 +17,7 @@ class RsvpOnline:
 
         self.__device = []
         self.__show_stimulus_thread = None
-        self.__model = None
+        self.__model = Net()
 
         self.fixation_duration = -1
         self.pic_duration = -1
@@ -26,27 +28,28 @@ class RsvpOnline:
 
         self.time_window = 1
 
+    # noinspection DuplicatedCode
     def add_device(self, device_info):
         """
         添加设备
         :param device_info: 设备信息
         :return:
         """
+        print(device_info)
 
-        # 清空设备，清除前一次设备添加失败的影响
-        self.__device = []
+        device = None
 
         # 添加设备
-        for i in range(device_info['num']):
-            if device_info['type'][i] == 'neuroscan':
-                self.__device.append(NeuroScan())
+        if device_info['type'] == 'neuroscan':
+            device = NeuroScan()
 
         # 连接设备
-        for i in range(device_info['num']):
-            self.__device[i].connect(device_info['ip'][i], device_info['port'][i])
-            # NeuroScan设备连接完毕后开始播放
-            if device_info['type'][i] == 'neuroscan':
-                self.__device[i].start_acquisition()
+        device.connect(device_info['ip'], device_info['port'])
+        # NeuroScan设备连接完毕后开始播放
+        if device_info['type'] == 'neuroscan':
+            device.start_acquisition()
+
+        self.__device.append(device)
 
     def set_study(self, study_info):
         """
@@ -85,13 +88,18 @@ class RsvpOnline:
             for dv_id in range(len(self.__device)):
                 self.__request_show_sti_pic(pic=self.pic_list[i])  # 向前端发送刺激图片
                 # 预测该刺激图片属于哪一类
-                __predict_thread = Thread(target=self.__model_predict, args=(i, time.time(), ))
+                __predict_thread = Thread(target=self.__model_predict, args=(i, time.time(),))
                 __predict_thread.start()
 
                 print(time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()), self.pic_list[i])
                 time.sleep(self.pic_duration)
 
         self.__request_show_end_pic()
+
+        # 断开设备连接
+        for i in range(len(self.__device)):
+            self.__device[i].stop_send_data()
+            self.__device[i].disconnect()
 
     def __request_show_fixation_pic(self):
         requests.get(url=self.__websocket_url + 'sendfixpic')
@@ -109,9 +117,9 @@ class RsvpOnline:
             if isinstance(self.__device[i], NeuroScan):
                 data.append(self.__device[i].get_eeg_by_time(timestamp, self.time_window))
 
-        res = []
+        res = {}
         # 模型预测
         for i in range(len(data)):
-            res.append({'pic_idx': idx, 'predict': self.__model(data[i])})
+            res = {'pic_idx': idx, 'predict': self.__model(data[i])}
 
         requests.post(url=self.__websocket_url + 'sendpredict', json=json.dumps(res))
